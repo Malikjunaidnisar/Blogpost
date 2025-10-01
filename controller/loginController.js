c6bd5fc1d37b3303f69c7b911824de99b0fe28df
Malikjunaidnisar <sayaandtrain1@gmail.com>
2025-10-01 23:40:05 +0500

    Update loginController.js

--- controller/loginController.js
+++ controller/loginController.js
@@ -16,7 +16,7 @@ loginRouter
 
 })
 
-/*.post('/',async(req,res)=>{
+.post('/',async(req,res)=>{
 	try{
 		
 	let user = await User.find({userEmail:req.body.email})
@@ -39,6 +39,6 @@ loginRouter
 	}
 
 
-})*/
+})
 
 module.exports={loginRouter}
